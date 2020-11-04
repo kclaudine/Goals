e01f153ddfc37978aa2f7e4f2b13bbd359199bc3
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2020-11-04 23:28:53 +0200

    changes

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+  goals: Goal[];
+
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  completeGoal(isComplete, index){
+    if (isComplete) {
+      this.goals.splice(index,1);
+    }
+  }
+
+  deleteGoal(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
+    }
+  }
+  
+
+
+  constructor(goalService:GoalService) { 
+    this.goals = goalService.getGoals()
+  }
+
+  ngOnInit() {
+  }
+
+}
